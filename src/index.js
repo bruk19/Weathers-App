a4aee0cc0d4b35e1de9b4bfeb55109fa3803c778
bruk19 <brukteshome21@gmail.com>
2022-07-21 18:17:04 +0300

    Add a method fetch to get data from weathermap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import _ from 'lodash';
 import './style.css';
+import {
+  displayData, displayError, changeToCelcius, changeToFerheneit,
+} from './dom';
+import { clearField } from './logic';
 
-function component() {
-  const element = document.createElement('div');
+const cityWeather = (city_name = 'Addis Ababa') => {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=665d996c0995235e2a6538dc312d1922`, {
+    method: 'GET',
+    mode: 'cors',
+  })
+    .then((response) => {
+      if (!response.ok) {
+        displayError();
+      }
+      return response.json();
+    }).then((result) => {
+      displayData(result);
+      changeToFerheneit(result);
+      changeToCelcius(result);
+    }).catch(() => {
 
-  return element;
-}
-
-document.body.appendChild(component());
+    });
+};
